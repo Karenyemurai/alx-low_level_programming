981ec0222103239149ea8ab26b8117762c357c87
Karenyemurai <karen.nyahunzvi@gmail.com>
2023-03-16 17:28:38 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * print:str:"Programming is like building a multilingual puzzle
+ * Return: 0 (Success)
+ */
+
+
+int main(void)
+{
+	 puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
