6ed31ea3ab42c33a5799261969c1723edb5bc2b9
Karenyemurai <karen.nyahunzvi@gmail.com>
2023-03-16 18:43:53 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,8 @@
  * main - Entry point
  * Return: 0 (Success)
  */
-
 int main(void)
 {
-	 pintf("with proper grammar, but the outcome is a piece of art,\n")
-
-	return (0);
+pintf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
 }
